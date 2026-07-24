9e5f3d68a51a8af93c41c6f4909e795428857ed5
Michael R <89427972+Michael-RDev@users.noreply.github.com>
2026-07-24 17:38:20 -0400

    Solve transformers-positional-encoding

---
+++ transformer/transformers-positional-encoding/transformers-positional-encoding.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def positional_encoding(seq_length: int, d_model: int) -> np.ndarray:
+    pe = np.zeros((seq_length, d_model))
+    base = 10000
+    position = np.arange(seq_length)[:, np.newaxis]
+    div_term = np.exp(np.arange(0, d_model, 2) * -np.log(base) / d_model)
+    pe[:, 0::2] = np.sin(position * div_term)
+    pe[:, 1::2] = np.cos(position * div_term)
+    return pe
